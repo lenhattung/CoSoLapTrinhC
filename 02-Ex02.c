ec7372b02ba689c41f181d35f8d972c4dc0ee52a
Le Nhat Tung <lenhattung@gmail.com>
2026-05-02 14:33:14 +0700

    u
    u

---
+++ 02-Ex02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    printf("%c", "\a");
+    printf("THIEP MOI: \n");
+    printf("Than moi ban: \"Nguyen Van A\" \n");
+    printf("Toi du dam cuoi cua minh \n");
+    printf("Vao luc 19h ngay 20/10/2030\n");
+    printf("Tai: 01/02 Vinh Vien - TP. HCM\n");
+    printf("Rat han hanh don tiep\n");
+    return 0;
+}
---
+++ 03-Ex03.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+    printf("Nhap so nguyen x = ");
+    scanf("%d", &x);
+    printf("So nguyen vua nhap la: %d", x);
+    return 0;
+}    
